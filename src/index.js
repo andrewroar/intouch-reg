2d663ca94d67320a6252c0ebdd39c93ad06c2bf5
andrewroar <59775822+andrewroar@users.noreply.github.com>
2021-05-04 12:03:49 +0100

    change css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "./components/css/signup.css";
+
+import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
